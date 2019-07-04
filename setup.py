5141d1c71405d4307d9fd5ee38a11892e6e1bca8
CarmenLegarre <52545364+CarmenLegarre@users.noreply.github.com>
2019-07-04 22:18:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+ 
+setup(
+    name='cirve',
+    packages=['cirve'], # Mismo nombre que en la estructura de carpetas de arriba
+    version='0.1',
+    license='GNU GPLv3 ', # La licencia que tenga tu paqeute
+    description='Software for circuits analysis and solving.',
+    author='CarmenLegarre',
+    author_email='Carmenlegarreta@hotmail.com',
+    url='https://github.com/RDCH106/mypackage', # Usa la URL del repositorio de GitHub
+    download_url='https://github.com/RDCH106/parallel_foreach_submodule/archive/v0.1.tar.gz', # Te lo explico a continuación
+    keywords='circuits, solving', # Palabras que definan tu paquete
+    classifiers=['Programming Language :: Python :: 3.7'],
+)
